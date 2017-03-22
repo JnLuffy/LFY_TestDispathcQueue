610c0b74cdf3428da32c30e8c830361fbe5df651
luffy <lushihong2012@163.com>
2017-03-22 22:24:51 +0800

    测试GCD

--- LFY_TestDispathcQueue/ViewController.m
+++ LFY_TestDispathcQueue/ViewController.m
@@ -17,7 +17,94 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self testSerialDisPatchQueue];
+    [self testConcurrentDisPatchQueue];
+    
+    
 }
+
+-(void)testSerialDisPatchQueue{
+    dispatch_queue_t queue = dispatch_queue_create("lfy", NULL);
+    dispatch_async(queue,^{
+        NSLog(@"1");
+    });
+    dispatch_async(queue,^{
+        NSLog(@"2");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"3");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"4");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"5");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"6");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"7");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"8");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"9");
+    });
+    
+    
+}
+
+
+-(void)testConcurrentDisPatchQueue{
+    dispatch_queue_t queue = dispatch_queue_create("lfy", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue,^{
+        NSLog(@"1");
+    });
+    dispatch_async(queue,^{
+        NSLog(@"2");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"3");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"4");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"5");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"6");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"7");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"8");
+    });
+    
+    dispatch_async(queue,^{
+        NSLog(@"9");
+    });
+    
+    
+}
+
 
 
 - (void)didReceiveMemoryWarning {
